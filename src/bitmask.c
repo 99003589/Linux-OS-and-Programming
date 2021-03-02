ace49aecfaa77fdbf9d78c8e0fb2b8e67627788a
99003589 <78540626+99003589@users.noreply.github.com>
2021-03-02 08:48:27 +0530

    Update bitmask.c

--- src/bitmask.c
+++ src/bitmask.c
@@ -1,1 +1,25 @@
+#include "bitmask.h"
+
+//Function to set bit
+int set(int m, int n) 
+{ 
+    return (m | (1 << (n - 1))); 
+} 
+  
+// Function to reset the nth bit of m 
+int reset(int m, int n) 
+{ 
+    return (m & (~(1 << (n - 1)))); 
+} 
+  
+// Function to toggle the nth bit of m 
+int flip(int m, int n)
+{ 
+    return (m ^ (1 << (n - 1))); 
+} 
+
+int query(int m,int n)
+{
+
+}
 
