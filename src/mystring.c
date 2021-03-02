3ca33adb6bba3bec4ed3fd10f449b4fd2186f923
99003589 <78540626+99003589@users.noreply.github.com>
2021-03-02 08:49:04 +0530

    Add files via upload

---
+++ src/mystring.c
@@ -0,0 +1,14 @@
+#include "mystring.h"
+int mystrlen(const char *str){
+    return strlen(str);
+}
+char* mystrcpy(char* destination, const char* source){
+    return strcpy(destination,source);
+}
+char* mystrcat(char *destination, const char *source){
+    return strcat(destination,source);
+}
+int mystrcmp(const char *str1, const char *str2){
+    return strcmp(Str1,Str2);
+}
+
